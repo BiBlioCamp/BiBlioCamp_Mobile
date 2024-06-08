6ed49e092036d641289eaabacf414c0f0f56d341
Jerso06 <jedsonhenrique2006@gmail.com>
2024-06-08 11:59:51 -0300

    Ajustes SingleChildScrollView do cadastrar

--- lib/cadaster.dart
+++ lib/cadaster.dart
@@ -203,7 +203,7 @@ class _CadasterState extends State<Cadaster> {
             ),
             child: Center(
               child: Padding(
-                padding: EdgeInsets.fromLTRB(50, 10, 50, 0),
+                padding: EdgeInsets.fromLTRB(50, 20, 50, 138),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
